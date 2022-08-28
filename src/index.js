16582ea17e71181fc97639942554d40408e52d1c
LEVI <atf343069@gmail.com>
2022-08-28 07:24:47 +0200

    test new repo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+ const express= require("express")
+ const apiServer=express()
+
+ apiServer.get('/', function(req,res){
+    res.send("hello from express")
+ })
+
+ apiServer.listen(3000,()=> console.log("api server started"));
